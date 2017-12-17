19a22b879063332c386bfa8ef737e846dbe61975
vzaichkinwork <34230333+vzaichkinwork@users.noreply.github.com>
2017-12-17 18:01:07 +0200

    массивы, фукнции

--- index.php
+++ index.php
@@ -1,12 +1,37 @@
 <?php
-	#$b = true;
+	$array = [1, 2, 3, -5, 23];
 
-	$a = 10;
-	$b = 10;
+	sort($array);
 
-	if ($b)
-	{
-		echo "this is true";
+	function custom_array_sum($arr){
+		$res = 0;
+
+		foreach ($arr as $value) {
+			$res += $value;
+		}
+
+		return $res;
 	}
+
+	echo custom_array_sum($array);
+
+	/*for ($i = 0;  $i < count($array); $i++) 
+	{ 
+		echo $array[$i] . '</br>';
+	}
+*/
+	/*echo 'before loop <br>';
+
+	foreach ($array as $elem) 
+	{
+		if ($elem % 2 == 0)
+		{
+			continue;
+		}
+		echo $elem . '</br>';
+	}
+
+	echo 'after loop <br>';*/
+
 
 ?>
