3cee72d481d723bcc46ab4b9060d0fd277d5dbef
Vitalii <vitaliyzaichkin@gmail.com>
2017-12-18 00:09:05 +0200

    функция фильтра нечетных чисел массива и вывод массива нечетных чисел

--- homework/filter_function.php
+++ homework/filter_function.php
@@ -1,1 +1,33 @@
 <?php
+    $array = [1, 2, 3, 4, 5, 6];
+
+
+    echo '<pre>';
+    echo "Оригинальный массив" . "</br>";
+    print_r($array);
+    echo '</pre>';
+
+    function filter_odd($arr){
+        foreach ($arr as $num){
+            if($num % 2 == 0){
+                echo $num . " - четное число" . "</br>";
+            }else{
+                $array_odd_num[] = $num;
+            }
+        }
+
+        echo '<pre>';
+        echo "Массив нечетных чисел" . "</br>";
+        print_r($array_odd_num);
+        echo '</pre>';
+
+        return $array_odd_num;
+    }
+
+    filter_odd($array);
+
+    /*echo '<pre>';
+    print_r($array);
+    echo '</pre>';*/
+
+?>
--- homework/table_friends.php
+++ homework/table_friends.php
@@ -6,22 +6,21 @@
  <body>
  <?php
 
- // вытащил содерживмое файла
- $friends = file_get_contents("db/friends.txt");
+    // вытащил содерживмое файла
+    $friends = file_get_contents("db/friends.txt");
 
- // создал массив из строк
- $friends_as_strings = explode("\n", $friends);
+    // создал массив из строк
+    $friends_as_strings = explode("\n", $friends);
 
-foreach ($friends_as_strings as $friend)
-{
-    $name_phone = explode("|", $friend);
-    echo '<table border="1" cellspacing="0" width="150">
-            <tr>
-                <td width="70">'.$name_phone[0].'</td>
-                <td>'.$name_phone[1].'</td>
-            </tr>
-         </table>';
-}
+    foreach ($friends_as_strings as $friend) {
+        $name_phone = explode("|", $friend);
+        echo '<table border="1" cellspacing="0" width="150">
+                <tr>
+                    <td width="70">'.$name_phone[0].'</td>
+                    <td>'.$name_phone[1].'</td>
+                </tr>
+            </table>';
+    }
  ?>
 
  </body>
