93f4f83e753d68ade5ebc35b3acbf9f914cd5603
Your Name <you@example.com>
2017-12-27 17:03:24 +0200

    table friends update

--- homework2/table_friends.php
+++ homework2/table_friends.php
@@ -1,27 +1,17 @@
-<html>
- <head>
-   <title>Таблица друзей</title>
-   <meta content="text/html; charset=utf-8">
- </head>
- <body>
- <?php
-
+<?php
+    header('Content-type: text/html; charset=utf-8');
     // вытащил содерживмое файла
     $friends = file_get_contents("db/friends.txt");
 
     // создал массив из строк
     $friends_as_strings = explode("\n", $friends);
-
+    echo '<table border="1" cellspacing="0" width="150">';
     foreach ($friends_as_strings as $friend) {
         $name_phone = explode("|", $friend);
-        echo '<table border="1" cellspacing="0" width="150">
-                <tr>
-                    <td width="70">'.$name_phone[0].'</td>
-                    <td>'.$name_phone[1].'</td>
-                </tr>
-            </table>';
+        echo '<tr>
+                 <td width="70">'.$name_phone[0].'</td>
+                 <td>'.$name_phone[1].'</td>
+              </tr>';
     }
- ?>
+    echo '</table>';
 
- </body>
-</html>
